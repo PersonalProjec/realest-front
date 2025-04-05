92945aba564493dc83326cac1c38f7a4e0cfbfb8
PersonalProjec <personalprojects@workwithwilton.com>
2025-04-05 16:25:31 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import './index.css';
+import App from './App.jsx';
 
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+if (!clientId) {
+  console.error('Google Client ID is not defined in environment variables.');
+}
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <GoogleOAuthProvider clientId={clientId}>
+      <App />
+    </GoogleOAuthProvider>
+  </StrictMode>
+);
