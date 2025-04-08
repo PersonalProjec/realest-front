0df8bdc5e2d886422180c11020b92e5ebcae770d
PersonalProjec <personalprojects@workwithwilton.com>
2025-04-08 16:14:52 +0100

    Update index.js

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -4,11 +4,6 @@ import house2 from './house2.avif'
 import house3 from './house3.jpg'
 import house4 from './house4.jpg'
 import house5 from './house5.jpg'
-import house1 from './house1.avif'
-import house2 from './house2.avif'
-import house3 from './house3.jpg'
-import house4 from './house4.jpg'
-import house5 from './house5.jpg'
 import house6 from './house6.jpg'
 import testy1 from './testy1.jpg'
 import testy2 from './testy2.jpg'
@@ -21,11 +16,6 @@ import luxury5 from './luxury5.jpg'
 import luxury6 from './luxury6.jpg'
 import luxury7 from './luxury7.jpg'
 import luxury8 from './luxury8.jpg'
-
-import house6 from './house6.jpg'
-import testy1 from './testy1.jpg'
-import testy2 from './testy2.jpg'
-import testy3 from './testy3.jpg'
 
 export {
   house,
@@ -46,19 +36,4 @@ export {
   luxury6,
   luxury7,
   luxury8,
-}
-
-
-
-export {
-  house,
-  house1,
-  house2,
-  house3,
-  house4,
-  house5,
-  house6,
-  testy1,
-  testy2,
-  testy3
 }
